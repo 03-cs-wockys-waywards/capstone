bd10cb15496640e718305f07fd9e10c48b1f15cd
Karen <devkarenc@gmail.com>
2021-07-03 18:08:52 -0400

    Style the status bar to dark-content on white background

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react';
+import { View, SafeAreaView, Platform, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { firebase } from './src/firebaseSpecs/config';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -17,6 +19,31 @@ const Stack = createStackNavigator();
 const screenOptions = {
   cardStyle: { backgroundColor: 'white' },
 };
+const MyStatusBar = ({ backgroundColor, ...props }) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <SafeAreaView>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </SafeAreaView>
+  </View>
+);
+
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    height: APPBAR_HEIGHT,
+  },
+  content: {
+    flex: 1,
+  },
+});
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -62,6 +89,7 @@ export default function App() {
           </>
         )}
       </Stack.Navigator>
+      <MyStatusBar backgroundColor="white" barStyle="dark-content" />
     </NavigationContainer>
   );
 }
