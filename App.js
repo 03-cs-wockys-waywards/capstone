a9896ba2388375fcea007da358f3b2578ae4b0ea
Victoria Kim <vkim20@gmail.com>
2021-07-09 17:31:36 -0400

    Navigation (#49)
    * editing navigation
    
    * fix: navigation streamlined

--- App.js
+++ App.js
@@ -8,54 +8,46 @@ import {
   Text,
   Button,
 } from 'react-native'
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
 import { firebase } from './src/firebaseSpecs/config'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import {
   LoginScreen,
-  HomeScreen,
   RegistrationScreen,
   ProfileStepOne,
   ProfileStepTwo,
   ProfileStepThree,
   Confirmation,
-} from './src/screens';
-import { decode, encode } from 'base-64';
-import { Provider } from 'react-redux';
-import store from './src/store';
+} from './src/screens'
+import MainScreen from './src/Main'
+import { decode, encode } from 'base-64'
+import { Provider } from 'react-redux'
+import store from './src/store'
 
 if (!global.btoa) {
-  global.btoa = encode;
+  global.btoa = encode
 }
 if (!global.atob) {
-  global.atob = decode;
+  global.atob = decode
 }
 
 const Stack = createStackNavigator()
-const Tab = createMaterialBottomTabNavigator()
-
-// Placeholder screen for testing
-const EmptyScreen = () => {
-  return null
-}
 
 const screenOptions = {
   cardStyle: { backgroundColor: 'white' },
-};
+}
 const MyStatusBar = ({ backgroundColor, ...props }) => (
   <View style={[styles.statusBar, { backgroundColor }]}>
     <SafeAreaView>
       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
     </SafeAreaView>
   </View>
-);
+)
 
-const STATUSBAR_HEIGHT = StatusBar.currentHeight;
-const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+const STATUSBAR_HEIGHT = StatusBar.currentHeight
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56
 
 const styles = StyleSheet.create({
   container: {
@@ -70,104 +62,68 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
   },
-});
+})
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    const usersRef = firebase.firestore().collection('users');
+    const usersRef = firebase.firestore().collection('users')
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         usersRef
           .doc(user.uid)
           .get()
           .then((document) => {
-            const userData = document.data();
-            setLoading(false);
-            setUser(userData);
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
           })
           .catch((error) => {
-            setLoading(false);
-          });
+            setLoading(false)
+          })
       } else {
-        setLoading(false);
+        setLoading(false)
       }
-    });
-  }, []);
+    })
+  }, [])
 
   if (loading) {
-    return <></>;
+    return <></>
   }
 
   return (
     <Provider store={store}>
       <NavigationContainer>
-        {user ? (
-          <Tab.Navigator
-            initialRouteName="Home"
-            activeColor="#d7f81e"
-            inactiveColor="#e4dbff"
-            labeled={false}
-            labelStyle={{ fontSize: 12 }}
-            barStyle={{ backgroundColor: '#106563' }}
-          >
-            <Tab.Screen
-              name="Search"
-              component={EmptyScreen}
-              options={{
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons
-                    name="magnify"
-                    color={color}
-                    size={28}
-                  />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons name="home" color={color} size={28} />
-                ),
-              }}
-            />
-            <Tab.Screen
-              name="Profile"
-              component={EmptyScreen}
-              options={{
-                tabBarIcon: ({ color }) => (
-                  <MaterialCommunityIcons
-                    name="account"
-                    color={color}
-                    size={28}
-                  />
-                ),
-              }}
-            />
-          </Tab.Navigator>
-        ) : (
-          <Stack.Navigator headerMode="none" initialRouteName="Login">
-            {/* <Stack.Screen
+        <Stack.Navigator headerMode="none" screenOptions={screenOptions}>
+          {user ? (
+            <Stack.Screen name="Main" component={MainScreen} />
+          ) : (
+            <>
+              {/* <Stack.Screen
               name="Landing"
               component={LandingScreen}
               options={{ headerShown: false }}
             /> */}
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-            <Stack.Screen name="ProfileStepOne" component={ProfileStepOne} />
-            <Stack.Screen name="ProfileStepTwo" component={ProfileStepTwo} />
-            <Stack.Screen
-              name="ProfileStepThree"
-              component={ProfileStepThree}
-            />
-            <Stack.Screen name="Confirmation" component={Confirmation} />
-          </Stack.Navigator>
-        )}
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen
+                name="Registration"
+                component={RegistrationScreen}
+              />
+              <Stack.Screen name="ProfileStepOne" component={ProfileStepOne} />
+              <Stack.Screen name="ProfileStepTwo" component={ProfileStepTwo} />
+              <Stack.Screen
+                name="ProfileStepThree"
+                component={ProfileStepThree}
+              />
+              <Stack.Screen name="Confirmation" component={Confirmation} />
+              <Stack.Screen name="Main" component={MainScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+        <MyStatusBar />
       </NavigationContainer>
     </Provider>
-  );
+  )
 }
