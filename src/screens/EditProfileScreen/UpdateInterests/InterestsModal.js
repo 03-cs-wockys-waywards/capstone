03835db8271780c32b20e1f6a9ef4b60682669c1
Fernanda Penfold <fpenfold723@gmail.com>
2021-07-12 13:52:26 -0400

    perf: fixes bug so that editPronouns dropdown renders in front of other components and is clickable

--- src/screens/EditProfileScreen/UpdateInterests/InterestsModal.js
+++ src/screens/EditProfileScreen/UpdateInterests/InterestsModal.js
@@ -6,8 +6,9 @@ import {
   TouchableOpacity,
   Modal,
   FlatList,
+  StyleSheet,
 } from "react-native";
-import { styles } from "../UpdateImage/ImageModal";
+// import { styles } from "../UpdateImage/ImageModal";
 import allInterests from "../../SetUpProfileScreens/ProfileStepTwo/interestsArray";
 import { Pill } from "../../../components/Pill";
 import { getColorsArray } from "../../../helpers/getColorsArray";
@@ -63,12 +64,67 @@ export default function InterestsModal({ user, setUser }) {
           />
         </View>
       </Modal>
-      <TouchableOpacity
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.textStyle}>+ / -</Text>
-      </TouchableOpacity>
+      <View>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.textStyle}>+ / -</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
+
+export const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    margin: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginTop: 30,
+    marginBottom: 15,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
