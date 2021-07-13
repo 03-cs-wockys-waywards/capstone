df4a968567746e405c0d31679ac8f5593df2b4d0
Fernanda Penfold <fpenfold723@gmail.com>
2021-07-13 13:32:17 -0400

    feat:ProfileStack.Navigator adds user's name as title and profile settings icon as headerRight

--- src/screens/ProfileScreen/ProfileNavigator.js
+++ src/screens/ProfileScreen/ProfileNavigator.js
@@ -1,18 +1,35 @@
+import { useSelector } from "react-redux";
 import React from "react";
 import { Text } from "react-native";
 import { Icon } from "react-native-elements";
 import { createStackNavigator } from "@react-navigation/stack";
-import { rightIcons } from "../../components/HeaderRightIcons";
 import ProfileScreen from "./ProfileScreen";
+import { renderName } from "../../helpers";
 
 const ProfileStack = createStackNavigator();
 
 export default function ProfileNavigator({ navigation }) {
+  const { firstName, lastName } = useSelector((state) => state.user);
+
+  const getSettingsIcon = (navigation) => (
+    <Icon
+    type="material-community"
+    name="account-cog"
+    size={25}
+    onPress={() => navigation.navigate("EditProfile")}
+  />
+  );
+
   return (
     <ProfileStack.Navigator>
       <ProfileStack.Screen
         name="Profile"
         component={ProfileScreen}
+        options={() => ({
+            title: `${firstName} ${lastName[0]}.`,
+            // title: renderName(route),
+            headerRight: () => getSettingsIcon(navigation)
+        })}
       />
     </ProfileStack.Navigator>
   );
