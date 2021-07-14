fdd374322f86e67d7073c2eed7a432457fcb6bfc
Karen <devkarenc@gmail.com>
2021-07-14 16:06:53 -0400

    Enable search bar to filter through discover list by firstName

--- src/screens/HomeScreen/DiscoverList.js
+++ src/screens/HomeScreen/DiscoverList.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { SafeAreaView, FlatList } from 'react-native'
-import { fetchUsersWithInterests } from '../../store/discoverUsersReducer'
-import styles from './styles'
-import UserRow from './UserRow'
-import { firebase } from '../../firebaseSpecs/config'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { SafeAreaView, FlatList } from 'react-native';
+import { fetchUsersWithInterests } from '../../store/discoverUsersReducer';
+import styles from './styles';
+import UserRow from './UserRow';
+import { firebase } from '../../firebaseSpecs/config';
 
 export class DiscoverList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   // get current user's doc & interests
@@ -20,22 +20,22 @@ export class DiscoverList extends Component {
       .get()
       .then((snapshot) => {
         if (snapshot.exists) {
-          const user = snapshot.data()
-          const interests = user.interests
+          const user = snapshot.data();
+          const interests = user.interests;
           // set users with similar interests in the redux store
-          this.props.setInterests(interests)
+          this.props.setInterests(interests);
         } else {
-          console.log('user does not exist')
+          console.log('user does not exist');
         }
-      })
+      });
   }
 
   render() {
-    const { discoverUsers, navigation } = this.props
+    const { discoverUsers, navigation } = this.props;
 
     const renderItem = ({ item }) => (
       <UserRow item={item} navigation={navigation} />
-    )
+    );
 
     return (
       <SafeAreaView style={styles.listContainer}>
@@ -45,7 +45,7 @@ export class DiscoverList extends Component {
           renderItem={renderItem}
         />
       </SafeAreaView>
-    )
+    );
   }
 }
 
@@ -53,10 +53,10 @@ const mapState = (state) => ({
   user: state.user,
   likes: state.user.likes,
   discoverUsers: state.discoverUsers,
-})
+});
 
 const mapDispatch = (dispatch) => ({
   setInterests: (interests) => dispatch(fetchUsersWithInterests(interests)),
-})
+});
 
-export default connect(mapState, mapDispatch)(DiscoverList)
+export default connect(mapState, mapDispatch)(DiscoverList);
--- src/screens/SearchScreen/SearchScreen.js
+++ src/screens/SearchScreen/SearchScreen.js
@@ -1,31 +1,92 @@
 import React, { Component } from 'react';
-import { SafeAreaView, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import { SafeAreaView, View, FlatList } from 'react-native';
+import { fetchUsersWithInterests } from '../../store/discoverUsersReducer';
 import SearchBar from '../../components/SearchBar';
+import UserRow from '../HomeScreen/UserRow';
 
-export default class SearchScreen extends Component {
+export class SearchScreen extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
+      originalData: [],
+      searchData: [],
       searchTerm: '',
     };
+    this.updateSearchText = this.updateSearchText.bind(this);
+    this.filterResults = this.filterResults.bind(this);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.setState({ originalData: this.props.discoverUsers });
+    // console.log('original data', this.state.originalData);
+  }
 
   updateSearchText(term) {
-    console.log('searchTerm', term);
+    this.setState({ searchTerm: term });
+    this.filterResults(term);
+  }
+
+  filterResults(text) {
+    const tempResults = [...this.state.originalData];
+    const searchResults = tempResults.filter((user) => {
+      // const interests = user.interests.map((interest) =>
+      //   interest.toUpperCase()
+      // );
+      // console.log('interests inside filterResults', interests);
+      // const searchTerm = text.toUpperCase();
+      // console.log('searchTerm inside filterResults', searchTerm);
+      // console.log(
+      //   'interests.includes(searchTerm)',
+      //   interests.includes(searchTerm)
+      // );
+      // return interests.includes(searchTerm);
+      const firstName = user.firstName.toUpperCase();
+      console.log('firstName inside filterResults', firstName);
+      const searchTerm = text.toUpperCase();
+      console.log('searchTerm inside filterResults', searchTerm);
+      console.log(
+        'firstName.includes(searchTerm)',
+        firstName.includes(searchTerm)
+      );
+      return firstName.indexOf(searchTerm) > -1;
+    });
+    // console.log('searchResults inside filterResults', searchResults);
+    this.setState({ searchData: searchResults });
   }
 
   render() {
-    const { searchTerm } = this.state;
+    const { navigation } = this.props;
+    const { originalData, searchData, searchTerm } = this.state;
+
+    const renderItem = ({ item }) => (
+      <UserRow item={item} navigation={navigation} />
+    );
 
     return (
       <SafeAreaView>
         <SearchBar
           searchText={searchTerm}
-          updateSearchText={() => this.updateSearchText(searchTerm)}
+          updateSearchText={this.updateSearchText}
+        />
+        <FlatList
+          data={searchData}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
         />
       </SafeAreaView>
     );
   }
 }
+
+const mapState = (state) => ({
+  user: state.user,
+  likes: state.user.likes,
+  discoverUsers: state.discoverUsers,
+});
+
+const mapDispatch = (dispatch) => ({
+  setInterests: (interests) => dispatch(fetchUsersWithInterests(interests)),
+});
+
+export default connect(mapState, mapDispatch)(SearchScreen);
