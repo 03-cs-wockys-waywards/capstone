3761347bad57da34f789ade8effd82c0135ac03e
Karen <devkarenc@gmail.com>
2021-07-13 22:34:03 -0400

    Chat Feed Screen in progress

--- src/screens/ChatScreens/ChatFeedScreen.js
+++ src/screens/ChatScreens/ChatFeedScreen.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   ImageBackground,
@@ -8,6 +9,7 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
+import { firebase } from '../../firebaseSpecs/config';
 import ChatFeedRow from '../../components/ChatFeedRow';
 
 const dummyData = [
@@ -22,20 +24,63 @@ const dummyData = [
   },
 ];
 
-export default class ChatFeedScreen extends Component {
-  constructor() {
-    super();
+const db = firebase.firestore();
+const messagesRef = db.collection('messages');
+
+export default function ChatFeedScreen() {
+  const currentUser = useSelector((state) => state.user);
+  const [user, setUser] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    console.log('currentUser in ChatFeedScreen', currentUser);
+    const unsubscribe = messagesRef.onSnapshot((querySnapshot) => {
+      const messagesFirestore = querySnapshot
+        .docChanges()
+        .filter(({ type }) => type === 'added')
+        .map(({ doc }) => {
+          const message = doc.data();
+          return { ...message, createdAt: message.createdAt.toDate() };
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+      appendMessages(messagesFirestore);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const appendMessages = useCallback(
+    (messages) => {
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages)
+      );
+    },
+    [messages]
+  );
+
+  useEffect(() => {
+    console.log('user in ChatFeedScreen', user);
+    // for GiftedChat
+    setUser({
+      _id: currentUser.id,
+      name: currentUser.firstName,
+      avatar: currentUser.profilePicture,
+    });
+  }, [currentUser]);
+
+  async function handleSend(messages) {
+    const writes = messages.map((m) => messagesRef.add(m));
+    await Promise.all(writes);
   }
 
-  render() {
-    return (
-      <SafeAreaView>
-        <View>
-          {dummyData.map((dummy, index) => (
-            <ChatFeedRow key={index} firstName={dummy.firstName} />
-          ))}
-        </View>
-      </SafeAreaView>
-    );
-  }
+  return (
+    <SafeAreaView>
+      <View>
+        {/* {dummyData.map((dummy, index) => (
+          <ChatFeedRow key={index} firstName={dummy.firstName} />
+        ))} */}
+        <Text>Chat Feed</Text>
+        <GiftedChat messages={messages} user={user} onSend={handleSend} />
+      </View>
+    </SafeAreaView>
+  );
 }
