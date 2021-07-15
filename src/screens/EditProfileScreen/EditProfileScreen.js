85aebb2fe0d21e6e7ba11040e1a5c788e758f501
Torie <vkim20@gmail.com>
2021-07-15 10:50:10 -0400

    feat: can edit profile & update firebase

--- src/screens/EditProfileScreen/EditProfileScreen.js
+++ src/screens/EditProfileScreen/EditProfileScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, ScrollView } from 'react-native'
+import { SafeAreaView, ScrollView, TouchableOpacity, Text } from 'react-native'
+import { firebase } from '../../firebaseSpecs/config'
 import styles from './styles'
 import { useSelector, useDispatch } from 'react-redux'
 import UpdateImage from './UpdateImage/UpdateImage'
@@ -8,25 +9,44 @@ import UpdatePronouns from './UpdatePronouns'
 import UpdateInterests from './UpdateInterests/UpdateInterests'
 import { editUserInfo } from '../../store/userReducer'
 
-export default function EditProfile() {
+export default function EditProfile({ navigation }) {
   const _user = useSelector((state) => state.user)
   const [user, setUser] = useState(_user)
 
   const dispatch = useDispatch()
 
   useEffect(() => {
-    //console.log('User BEFORE dispatch >>>>> ', user)
+    //console.log('User BEFORE dispatch >>>>> ', _user.interests)
     dispatch(editUserInfo(user))
-    //console.log('User AFTER dispatch >>>>> ', user)
   }, [user])
 
+  //console.log('User AFTER dispatch/useEffect >>>>> ', _user.interests)
+
+  const updateFirebase = () => {
+    const uid = _user.id
+    const data = _user
+    const usersRef = firebase.firestore().collection('users')
+    usersRef
+      .doc(uid)
+      .set(data)
+      .then(() => {
+        navigation.popToTop()
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
+
   return (
-    <SafeAreaView styles={styles.container}>
-      <ScrollView>
+    <SafeAreaView>
+      <ScrollView styles={styles.container}>
         <UpdateImage user={user} setUser={setUser} />
         <UpdateName user={user} setUser={setUser} />
         <UpdatePronouns user={user} setUser={setUser} />
         <UpdateInterests user={user} setUser={setUser} />
+        <TouchableOpacity style={styles.button} onPress={updateFirebase}>
+          <Text style={styles.buttonText}>Confirm Changes</Text>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   )
