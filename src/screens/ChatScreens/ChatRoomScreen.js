5db7d0e5f5127c0765926ae43d43b5a5e5af9489
fernandapenfold <79024252+fernandapenfold@users.noreply.github.com>
2021-07-15 11:25:59 -0400

    Chat room (#92)
    * feat:ChatFeedScreen adds messageStore that organizes messages by sender
    
    * feat:ChatFeedScreen messageStore sorts messages by from and createdAt fields
    
    * feat:ChatFeedScreen retrieves potential match data and filters it to return match data
    
    * feat:ChatFeedScreen adds matchesStore that stores user ids as keys and user objects as values
    
    * feat:ChatFeedScreen adds function that returns an array of objects, each of which consists of a unique userId and message sent by that user
    
    * feat:ChatFeedScreen renders messages from matches to user, grouped by sender
    
    * chore:ChatFeedScreen cleans up code
    
    * feat:ChatFeedScreen clicking on chat bubble in chat feed takes you to chat room showing messages between you and the selected user
    
    * feat:ChatRoomScreen shows match's name as the chatroom header and renders messages between match and logged-in user
    
    * perf: adds conditional styling to chat bubbles
    
    * fix: created messages redux store. thunk in chatRoomScreen used to retrieve messages to user
    
    * fix: ChatRoomScreen has access to messages between logged in user and a selected match
    
    * fix: renders messages between current user and a selected match
    
    * bug: chatRoomScreen doesn't show chats in real time. only renders messages that are retrieved from firestore in useEffect
    
    * chore: cleans up code
    
    Co-authored-by: Karen <devkarenc@gmail.com>

--- src/screens/ChatScreens/ChatRoomScreen.js
+++ src/screens/ChatScreens/ChatRoomScreen.js
@@ -1,48 +1,40 @@
-import React, { useState } from "react";
-import { firebase } from "../../firebaseSpecs/config";
-import "firebase/firestore";
-import "firebase/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-import { useSelector } from "react-redux";
-import { SafeAreaView, ScrollView, View, Text, TextInput, TouchableOpacity } from "react-native";
-import ChatBubble from "../../components/ChatBubble";
-import styles from "./styles";
-
-const dummyData = [
-  {
-    avatar:
-      "https://images.pexels.com/photos/4001552/pexels-photo-4001552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    firstName: "Leslie",
-    lastName: "Heather",
-    user: "currentUser",
-    message: "Nice to chat with you!",
-  },
-  {
-    avatar:
-      "https://images.pexels.com/photos/2286385/pexels-photo-2286385.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    firstName: "Jenny",
-    lastName: "Kim",
-    user: "match",
-    message: "Do you have any plans this weekend?",
-  },
-];
+import React, { useState, useEffect } from 'react';
+import { firebase } from '../../firebaseSpecs/config';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  fetchMessagesToUser,
+  fetchMessagesFromUser,
+} from '../../store/messagesReducer';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import ChatBubble from '../../components/ChatBubble';
+import styles from './styles';
 
 export default function ChatFeedScreen({ route }) {
+  // console.log("----------------  IN CHAT ROOM  ----------------");
   const { match } = route.params;
   const user = useSelector((state) => state.user);
-  const [text, setText] = useState("");
+  const { toUser, fromUser } = useSelector((state) => state.messages);
+  const messages = [...toUser, ...fromUser];
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
 
-  const messagesRef = firebase.firestore().collection("messages");
-  const query = messagesRef
-    .where("to", "==", user.id)
-    .where("from", "==", match.id);
-  const _query = messagesRef
-    .where("to", "==", match.id)
-    .where("from", "==", user.id);
+  const messagesRef = firebase.firestore().collection('messages');
 
-  const [messagesToUser] = useCollectionData(query);
-  const [messagesFromUser] = useCollectionData(_query);
-  // const messages = [ ...messagesToUser, messagesFromUser ]
+  useEffect(() => {
+    dispatch(fetchMessagesToUser(user.id, match.id));
+    dispatch(fetchMessagesFromUser(user.id, match.id));
+  }, []);
+
+  // console.log("messages >>>>>", messages);
 
   const sendMessage = async () => {
     const { id, profilePicture } = user;
@@ -55,36 +47,30 @@ export default function ChatFeedScreen({ route }) {
       to: match.id,
     });
 
-    setText("");
+    setText('');
   };
-
-  console.log(
-    `messages to Rhetta from ${match.firstName} >>>>`,
-    messagesToUser
-  );
-  console.log(
-    `message to ${match.firstName} from Rhetta >>>>>`,
-    messagesFromUser
-  );
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollContainer}>
         <View style={styles.headerContainer}>
-          <Text style={styles.headerText}>Chat with Match XXX</Text>
+          <Text
+            style={styles.headerText}
+          >{`${match.firstName} ${match.lastName[0]}.`}</Text>
         </View>
-        {dummyData.map((dummy, index) => (
-          <ChatBubble key={index} message={dummy.message} user={dummy.user} />
-        ))}
+        {messages &&
+          messages.map((message, index) => (
+            <ChatBubble
+              key={index}
+              message={message.text}
+              user={message.from === user.id ? 'currentUser' : 'match'}
+            />
+          ))}
       </ScrollView>
-      <TextInput
-          onChangeText={setText}
-          value={text}
-          placeholder="Message"
-        />
-        <TouchableOpacity onPress={() => sendMessage()}>
-          <Text>Send</Text>
-        </TouchableOpacity>
+      <TextInput onChangeText={setText} value={text} placeholder="Message" />
+      <TouchableOpacity onPress={() => sendMessage()}>
+        <Text>Send</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
