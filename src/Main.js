4c96e312cde9aabb3fdf33326601c00672199df9
Karen Choi <49035897+DevKarenC@users.noreply.github.com>
2021-07-15 11:35:05 -0400

    Search screen (#93)
    * Create SearchScreen and connect it to the bottom tab nav
    
    * Enable search bar to filter through discover list by firstName
    
    * Enable search bar to filter through discover list by interest
    
    * Render screen description when the user did not start searching yet
    
    * Add text to inform user no users with the interest were found
    
    * Render interest suggestions and modify logic for ListEmptyComponent
    
    * Display autocompleted interest options to users
    
    * Convert functions inside of render to be class methods
    
    * feat: find user in the discover list by interest
    
    * Clean up unused code
    
    * Fix navigation problem with tab navigator

--- src/Main.js
+++ src/Main.js
@@ -2,14 +2,16 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { HomeScreen, ProfileNavigator, ChatNavigator } from './screens';
+import {
+  HomeScreen,
+  SearchScreen,
+  ProfileNavigator,
+  ChatNavigator,
+  SearchNavigator,
+} from './screens';
 import { editUserInfo } from './store/userReducer';
 
 const Tab = createMaterialBottomTabNavigator();
-
-const EmptyScreen = () => {
-  return null;
-};
 
 export default function MainScreen(props) {
   const user = props.route.params.user;
@@ -42,7 +44,7 @@ export default function MainScreen(props) {
       />
       <Tab.Screen
         name="Search"
-        component={EmptyScreen}
+        component={SearchNavigator}
         options={{
           tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="magnify" color={color} size={28} />
