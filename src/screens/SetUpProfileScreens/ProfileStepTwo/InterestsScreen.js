6df0ab03bcdbcd2d8ad8068cc3963e89af7ea4b0
Karen <devkarenc@gmail.com>
2021-07-15 15:49:19 -0400

    Modify the styling for the header

--- src/screens/SetUpProfileScreens/ProfileStepTwo/InterestsScreen.js
+++ src/screens/SetUpProfileScreens/ProfileStepTwo/InterestsScreen.js
@@ -1,54 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
   View,
   FlatList,
   SafeAreaView,
   Text,
   TouchableOpacity,
-} from 'react-native'
-import { Icon } from 'react-native-elements'
-import { EmptyCircle, FilledCircle } from '../../../components/ProgressCircles'
-import interests from './interestsArray'
-import styles from './styles'
-import { useSelector, useDispatch } from 'react-redux'
-import { editUserInfo } from '../../../store/userReducer'
+} from 'react-native';
+import { Icon } from 'react-native-elements';
+import { EmptyCircle, FilledCircle } from '../../../components/ProgressCircles';
+import interests from './interestsArray';
+import styles from './styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { editUserInfo } from '../../../store/userReducer';
 
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
     <Text style={[styles.itemName, textColor]}>{item}</Text>
   </TouchableOpacity>
-)
+);
 
 export default function InterestsScreen({ navigation, route }) {
-  const { password } = route.params
+  const { password } = route.params;
 
-  const user = useSelector((state) => state.user)
-  const init = user.interests.length ? user.interests : []
-  const [selectedInterests, setSelectedInterests] = useState(init)
-  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user);
+  const init = user.interests.length ? user.interests : [];
+  const [selectedInterests, setSelectedInterests] = useState(init);
+  const dispatch = useDispatch();
 
   const handlePress = (item) => {
     if (selectedInterests.includes(item)) {
       setSelectedInterests(
         selectedInterests.filter((interest) => interest !== item)
-      )
+      );
     } else if (selectedInterests.length < 5) {
-      setSelectedInterests([...selectedInterests, item])
+      setSelectedInterests([...selectedInterests, item]);
     }
-  }
+  };
 
   const navigateToNext = () => {
     if (selectedInterests.length) {
-      dispatch(editUserInfo({ interests: selectedInterests }))
-      navigation.navigate('ProfileStepThree', { password })
+      dispatch(editUserInfo({ interests: selectedInterests }));
+      navigation.navigate('ProfileStepThree', { password });
     }
-  }
+  };
 
   const renderItem = ({ item }) => {
     const backgroundColor = selectedInterests.includes(item)
       ? '#6e3b6e'
-      : '#f9c2ff'
-    const color = selectedInterests.includes(item) ? 'white' : 'black'
+      : '#f9c2ff';
+    const color = selectedInterests.includes(item) ? 'white' : 'black';
 
     return (
       <Item
@@ -57,15 +57,15 @@ export default function InterestsScreen({ navigation, route }) {
         backgroundColor={{ backgroundColor }}
         textColor={{ color }}
       />
-    )
-  }
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.headerTitle}>What are your interests?</Text>
         <Text style={styles.headerText}>
-          Choose up to 5 options ➨ {selectedInterests.length}/5
+          You selected {selectedInterests.length} out of 5 interests.
         </Text>
       </View>
       <FlatList
@@ -88,5 +88,5 @@ export default function InterestsScreen({ navigation, route }) {
         </TouchableOpacity>
       </View>
     </SafeAreaView>
-  )
+  );
 }
--- src/screens/SetUpProfileScreens/ProfileStepTwo/styles.js
+++ src/screens/SetUpProfileScreens/ProfileStepTwo/styles.js
@@ -1,32 +1,38 @@
-import { StyleSheet, StatusBar } from "react-native";
+import { StyleSheet, StatusBar } from 'react-native';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    // marginTop: StatusBar.currentHeight || 0,
+    marginHorizontal: '5%',
+    // paddingTop: '10%',
   },
   headerContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 25,
-    paddingBottom: 10
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 41,
+    marginBottom: 10,
   },
   headerTitle: {
     fontSize: 30,
     fontWeight: '600',
+    lineHeight: 36,
+    marginBottom: 5,
   },
   headerText: {
-    fontSize: 20,
-    lineHeight: 36
+    fontSize: 16,
+    lineHeight: 19,
+    marginTop: 15,
+    marginBottom: 15,
   },
   item: {
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 8,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 50,
-    flex: 1
+    flex: 1,
   },
   itemName: {
     fontSize: 16,
