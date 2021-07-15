226f8680ce60118a3c03d55c5a3abb0fc2f1328b
Torie <vkim20@gmail.com>
2021-07-15 16:14:44 -0400

    added light colors array

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -18,6 +18,26 @@ export const colorMap = {
   Crimson: 'e8073f',
 }
 
+/* Light Colors for Pills */
+export const lightColors = [
+  '#D4F4E8',
+  '#D4F4DC',
+  '#D9F4D4',
+  '#E5F4D4',
+  '#F1F4D4',
+  '#F4ECD4',
+  '#F4E0D4',
+  '#F4D4D5',
+  '#F4D4E1',
+  '#F4D4ED',
+  '#F0D4F4',
+  '#E4D4F4',
+  '#D8D4F4',
+  '#D4DDF4',
+  '#D4E9F4',
+  '#D4F4F4',
+]
+
 /* Extended Array
   [({
     name: 'St Patricks Blue',
