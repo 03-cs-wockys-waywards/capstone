3d7ecfd91598364b7b48fcfde700a6274d163324
Fernanda Penfold <fpenfold723@gmail.com>
2021-07-17 09:57:12 -0400

    refactors back to functional component in order to be able to use react-firebase hooks; query result includes field with document id values

--- src/screens/ChatScreens/ChatFeedScreen.js
+++ src/screens/ChatScreens/ChatFeedScreen.js
@@ -13,84 +13,32 @@ import ChatFeedRow from "../../components/ChatFeedRow";
 import ChatRoomScreen from "./ChatRoomScreen";
 import styles from "./styles";
 
-export default class ChatFeedScreen extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      matches: [],
-      chatRooms: []
-    }
-  }
+// // QUERY FOR CHAT ROOM
+// const ChatRoomQuery = currentUser.id
+//   ? messagesRef.where(`users.${currentUser.id}`, "==", true).where("users.E1GPo5ZjsJWSvxwVjhpaEsXEHUj2", "==", true)
+//   : null;
+// const [chatRoomThread] = useCollectionData(ChatRoomQuery);
 
-  componentDidMount() {
-    const messagesRef = firebase.firestore().collection("messages");
-    const getChatRooms = currentUser.id
-      ? messagesRef.where(`users.${currentUser.id}`, "==", true)
-      : null;
-    const [chatRooms] = useCollectionData(getChatRooms);
-    this.setState({ ...this.state, chatRooms });
-    // NEED: array of matchIds
-    // HAVE: users: { user1Id: true, user2Id: true }
-  }
-  
+// const matchesIds = chatRoomThread.map((chatRoom) => {
+//   const { users } = chatRoom;
+//   return Object.keys(users).filter((user) => {
+//     return user.id !== currentUser.id
+//   });
+// });
 
-
-
-
-  // QUERY FOR CHAT FEED
-
-  // QUERY FOR CHAT ROOM
-  const ChatRoomQuery = currentUser.id
-    ? messagesRef.where(`users.${currentUser.id}`, "==", true).where("users.E1GPo5ZjsJWSvxwVjhpaEsXEHUj2", "==", true)
-    : null;
-  const [chatRoomThread] = useCollectionData(ChatRoomQuery);
-  // console.log("CHAT ROOM >>>>>>", chatRoomThread);
-
-  // console.log('CHAT ROOM THREAD >>>>>>', chatRoomThread)
-  const matchesIds = chatRoomThread.map((chatRoom) => {
-    const { users } = chatRoom;
-    return Object.keys(users).filter((user) => {
-      console.log("MATCH IN FILTER >>>>", user)
-      console.log("CURRENT USER IN FILTER >>>>>", currentUser);
-      return user.id !== currentUser.id
-    
-    });
+export default function ChatFeedScreen({ navigation }) {
+  const currentUser = useSelector((state) => state.user);
+  const messagesRef = firebase.firestore().collection("messages");
+  const query = currentUser.id ? messagesRef.where(`users.${currentUser.id}`, "==", true) : null;
+  const [chatRooms, loading, error] = useCollectionData(query, {
+    idField: "id"
   });
 
-
-  
-  
+  console.log(chatRooms)
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollContainer}>
-        {/* {messages &&
-          messages.map((message, index) => {
-            const match = matchesStore[message.senderId];
-            return match ? (
-              <ChatFeedRow
-                key={index}
-                avatar={null}
-                firstName={match.firstName}
-                lastName={match.lastName}
-                latestMessage={message.text}
-                handlePress={() => handlePress(match)}
-              />
-            ) : (
-              <></>
-            );
-          })} */}
-        {/* <Text>{threads[0].latestMessage.text}</Text> */}
-        {/* {threads && threads.map((thread, index) => {
-            const matchId = thread.users.filter((user) => user.id !== currentUser.id)[0];
-            const match = discoverStore[matchId];
-            console.log('matchId >>>>>>', matchId)
-            console.log('match >>>>>', match)
-            return (
-              <Text key={index}>TEST</Text>
-            );
-        })} */}
-
         {/* {threads.map((thread, index) => {
             const matchId = thread.users.filter((user) => user.id !== currentUser.id)[0];
             const match = matchesStore[matchId];
