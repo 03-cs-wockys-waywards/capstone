86e2d719c4354617dddcf0260e69689e651cd3a5
fernandapenfold <79024252+fernandapenfold@users.noreply.github.com>
2021-07-17 15:15:30 -0400

    queries for chat feed and chat room (#120)
    * feat:ProfileStack.Navigator adds user's name as title and profile settings icon as headerRight
    
    * feat:ChatFeedScreen adds query that retrieves message documents where the user is in the users field
    
    * adds chat room and and chat feed queries
    
    * refactoring ChatFeedScreen to class component
    
    * refactors back to functional component in order to be able to use react-firebase hooks; query result includes field with document id values
    
    * messages render with placeholder firstName and lastName params in ChatFeedRow; document id gets passed to handlePress
    
    * ChatRoomScreen receives documentId as route params from ChatFeedScreen
    
    * useEffect in ChatRoomScreen subscribes to updates in chat room document passed as params
    
    * messages in chat room document get set on local state
    
    * ChatRoomScreen renders messages in chat room doc
    
    * send messages updates in firestore in real time
    
    * sendMessage also updates latestMessage field
    
    * adds logic to check if a chatRomm exists when user clicks on chat icon in top navigation from SingleProfileScreen
    
    * creates a new document in the messages collection if none exists
    
    * navigates from SingleUserScreen to ChatRoom; loads chat room document if it exists, or creates a new document if none exists
    
    * cleans up code

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -1,23 +1,51 @@
-import React from 'react'
-import { Text, Image, StyleSheet } from 'react-native'
-import { Icon } from 'react-native-elements'
-import { createStackNavigator } from '@react-navigation/stack'
-import UsersList from './UsersList'
-import SingleUserProfile from '../SingleUserProfileScreen/SingleUserProfile'
-import headerLogo from '../../../assets/images/header-logo.png'
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import { Icon } from "react-native-elements";
+import { createStackNavigator } from "@react-navigation/stack";
+import UsersList from "./UsersList";
+import SingleUserProfile from "../SingleUserProfileScreen/SingleUserProfile";
+import headerLogo from "../../../assets/images/header-logo.png";
 import ChatRoomScreen from "../ChatScreens/ChatRoomScreen";
+import { firebase } from "../../firebaseSpecs/config";
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
 
-const HomeStack = createStackNavigator()
+const HomeStack = createStackNavigator();
 
 // replace with our actual logo
-const logo = () => <Image source={headerLogo} style={styles.logo} />
+const logo = () => <Image source={headerLogo} style={styles.logo} />;
 
 const renderName = (route) => {
-  return `${route.params.user.firstName} ${route.params.user.lastName[0]}.`
-}
+  return `${route.params.user.firstName} ${route.params.user.lastName[0]}.`;
+};
 
 const userChatIcon = (route, navigation) => {
+  let docId;
   const { user: match } = route.params;
+  const currentUserId = firebase.auth().currentUser.uid;
+  const messagesRef = firebase.firestore().collection("messages");
+  const query = currentUserId
+    ? messagesRef
+        .where(`users.${currentUserId}`, "==", true)
+        .where(`users.${match.id}`, "==", true)
+    : null;
+  const [value, loading, error] = useCollectionDataOnce(query, {
+    idField: "id",
+  });
+  if (!loading && value.length) {
+    const [chatRoom] = value;
+    docId = chatRoom.id;
+  } else {
+    const chatRoom = messagesRef.doc();
+    chatRoom.set({
+      users: {
+        [match.id]: true,
+        [currentUserId]: true,
+      },
+    });
+    docId = chatRoom.id;
+  }
+
+  console.log("chatRoomId in userChatIcon >>>>>", docId);
   return (
     <Icon
       type="material-community"
@@ -26,6 +54,7 @@ const userChatIcon = (route, navigation) => {
       onPress={() =>
         navigation.navigate("Chat Room", {
           match,
+          docId,
         })
       }
     />
@@ -40,12 +69,12 @@ export default function Home({ navigation }) {
         component={UsersList}
         options={{
           headerLeft: () => logo(),
-          headerTitle: '',
+          headerTitle: "",
           headerStyle: {
-            shadowColor: 'transparent',
+            shadowColor: "transparent",
             shadowRadius: 0,
             borderBottomWidth: 0,
-            backgroundColor: '#fff',
+            backgroundColor: "#fff",
           },
         }}
       />
@@ -61,16 +90,16 @@ export default function Home({ navigation }) {
         name="Chat Room"
         component={ChatRoomScreen}
         options={() => ({
-          title: '',
+          title: "",
         })}
       />
     </HomeStack.Navigator>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
-    shadowColor: 'transparent',
+    shadowColor: "transparent",
     shadowRadius: 0,
     shadowOffset: {
       height: 0,
@@ -78,8 +107,8 @@ const styles = StyleSheet.create({
   },
   logo: {
     flex: 1,
-    alignSelf: 'flex-start',
-    resizeMode: 'center',
+    alignSelf: "flex-start",
+    resizeMode: "center",
     marginLeft: -100,
   },
-})
+});
