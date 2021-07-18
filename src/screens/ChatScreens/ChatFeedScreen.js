fac4ea390f8350b3332d607110a7d3523b3574b9
Fernanda Penfold <fpenfold723@gmail.com>
2021-07-17 20:57:08 -0400

    filters chatFeed chat rooms to render only those that have messages

--- src/screens/ChatScreens/ChatFeedScreen.js
+++ src/screens/ChatScreens/ChatFeedScreen.js
@@ -1,44 +1,52 @@
-import { firebase } from '../../firebaseSpecs/config';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { firebase } from "../../firebaseSpecs/config";
+import "firebase/firestore";
+import "firebase/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   fetchPotentialMatches,
   setMatches,
-} from '../../store/potentialMatchesReducer';
-import { SafeAreaView, ScrollView, View, Text } from 'react-native';
-import ChatFeedRow from '../../components/ChatFeedRow';
-import ChatRoomScreen from './ChatRoomScreen';
-import styles from './styles';
+} from "../../store/potentialMatchesReducer";
+import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import ChatFeedRow from "../../components/ChatFeedRow";
+import ChatRoomScreen from "./ChatRoomScreen";
+import styles from "./styles";
 
-// // QUERY FOR CHAT ROOM
-// const ChatRoomQuery = currentUser.id
-//   ? messagesRef.where(`users.${currentUser.id}`, "==", true).where("users.E1GPo5ZjsJWSvxwVjhpaEsXEHUj2", "==", true)
-//   : null;
-// const [chatRoomThread] = useCollectionData(ChatRoomQuery);
-
-// const matchesIds = chatRoomThread.map((chatRoom) => {
-//   const { users } = chatRoom;
-//   return Object.keys(users).filter((user) => {
-//     return user.id !== currentUser.id
-//   });
-// });
+// THIS ISN'T WORKING & I CANT FIGURE OUT WHY
+// useEffect(() => {
+//   console.log('in useEffect')
+//   const messagesRef = firebase.firestore().collection("messages");
+//   messagesRef
+//     .where(`users.${currentUser.id}`, "==", true)
+//     .get()
+//     .then((snapshot) => {
+//       if (snapshot) {
+//         const chatRooms = snapshot.map((doc) => {
+//           const data = doc.data();
+//           return { ...data, id: doc.id }
+//         });
+//         setChatRooms(chatRooms)
+//       } else {
+//         console.log('no chat rooms found for this user');
+//       }
+//     })
+// }, []);
 
 export default function ChatFeedScreen({ navigation }) {
   const currentUser = useSelector((state) => state.user);
-  const messagesRef = firebase.firestore().collection('messages');
+  const messagesRef = firebase.firestore().collection("messages");
   const query = currentUser.id
-    ? messagesRef.where(`users.${currentUser.id}`, '==', true)
+    ? messagesRef.where(`users.${currentUser.id}`, "==", true)
     : null;
   const [chatRooms, loading, error] = useCollectionData(query, {
-    idField: 'id',
+    idField: "id",
   });
 
+  console.log("ChatRooms >>>>>", chatRooms);
+
   const handlePress = (id) => {
-    // console.log("ChatRoom documentID in handlePress >>>>", id)
-    navigation.navigate('ChatRoom', { docId: id });
+    navigation.navigate("ChatRoom", { docId: id });
   };
 
   return (
@@ -48,21 +56,23 @@ export default function ChatFeedScreen({ navigation }) {
           <></>
         ) : (
           chatRooms &&
-          chatRooms.map((chatRoom) => {
-            const { id, latestMessage } = chatRoom;
-            return (
-              <ChatFeedRow
-                key={id}
-                avatar={null}
-                firstName={'Placeholder'}
-                lastName={'X'}
-                latestMessage={
-                  latestMessage ? latestMessage.text : 'Start chatting!'
-                }
-                handlePress={() => handlePress(id)}
-              />
-            );
-          })
+          chatRooms
+            .filter((chatRoom) => chatRoom.messages)
+            .map((chatRoom) => {
+              const { id, latestMessage } = chatRoom;
+              return (
+                <ChatFeedRow
+                  key={id}
+                  avatar={null}
+                  firstName={"Placeholder"}
+                  lastName={"X"}
+                  latestMessage={
+                    latestMessage ? latestMessage.text : "Start chatting!"
+                  }
+                  handlePress={() => handlePress(id)}
+                />
+              );
+            })
         )}
       </ScrollView>
     </SafeAreaView>
