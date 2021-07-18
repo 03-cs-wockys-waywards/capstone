b27a6d24d1c871f91d6b76f19a7d1ceb8b745beb
Torie <vkim20@gmail.com>
2021-07-18 13:27:34 -0400

    refactored fetchUsersWithInterests thunk & added pull to refresh feature

--- src/screens/HomeScreen/DiscoverList.js
+++ src/screens/HomeScreen/DiscoverList.js
@@ -18,28 +18,39 @@ export class DiscoverList extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      isFetching: false,
+      interests: [],
+    }
+
     this.renderItem = this.renderItem.bind(this)
-    this.keyExtractor = this.keyExtractor.bind(this)
+    this.onRefresh = this.onRefresh.bind(this)
   }
 
   // get current user's doc & interests
   componentDidMount() {
-    firebase
-      .firestore()
-      .collection('users')
-      .doc(firebase.auth().currentUser.uid)
-      .get()
-      .then((snapshot) => {
-        if (snapshot.exists) {
-          const user = snapshot.data()
-          const interests = user.interests
-          // set users with similar interests in the redux store
-          this.props.setInterests(interests)
-        } else {
-          console.log('user does not exist')
-        }
-      })
+    // firebase
+    //   .firestore()
+    //   .collection('users')
+    //   .doc(firebase.auth().currentUser.uid)
+    //   .get()
+    //   .then((snapshot) => {
+    //     if (snapshot.exists) {
+    //       const user = snapshot.data()
+    //       const interests = user.interests
+    //       // set interests on state
+    //       this.setState({ interests })
+    //       // set users with similar interests in the redux store
+    //       this.props.setInterests(interests)
+    //     } else {
+    //       console.log('user does not exist')
+    //     }
+    //   })
+    this.props.getUsers()
+    this.setState({ isFetching: false })
   }
+
+  fetchInterests
 
   renderItem({ item }) {
     return <UserRow item={item} navigation={this.props.navigation} />
@@ -49,11 +60,22 @@ export class DiscoverList extends Component {
     return item.id.toString()
   }
 
+  onRefresh() {
+    //console.log('BEFORE fetch on refresh: ', this.state.isFetching)
+    this.setState({ isFetching: true }, () => {
+      //console.log('DURING fetch on refresh: ', this.state.isFetching)
+      this.props.getUsers()
+      this.setState({ isFetching: false })
+    })
+  }
+
   render() {
     const { renderItem, keyExtractor } = this
     const { user, users } = this.props
     // filter out the current user from discover list
     const discoverUsers = users.filter((person) => person.id !== user.id)
+
+    //console.log('fetch on refresh in RENDER: ', this.state.isFetching)
 
     return (
       <FlatList
@@ -62,6 +84,8 @@ export class DiscoverList extends Component {
         renderItem={renderItem}
         initialNumToRender={7}
         ListEmptyComponent={EmptyMessage}
+        onRefresh={() => this.onRefresh()}
+        refreshing={this.state.isFetching}
       />
     )
   }
@@ -73,7 +97,8 @@ const mapState = (state) => ({
 })
 
 const mapDispatch = (dispatch) => ({
-  setInterests: (interests) => dispatch(fetchUsersWithInterests(interests)),
+  // setInterests: (interests) => dispatch(fetchUsersWithInterests(interests)),
+  getUsers: () => dispatch(fetchUsersWithInterests()),
 })
 
 export default connect(mapState, mapDispatch)(DiscoverList)
--- src/store/discoverUsersReducer.js
+++ src/store/discoverUsersReducer.js
@@ -1,34 +1,45 @@
-import { firebase } from '../firebaseSpecs/config';
+import { firebase } from '../firebaseSpecs/config'
 
-const SET_DISCOVER_USERS = 'SET_ALL_USERS';
+const SET_DISCOVER_USERS = 'SET_ALL_USERS'
 
 export const setDiscoverUsers = (users) => ({
   type: SET_DISCOVER_USERS,
   users,
-});
+})
 
-export const fetchUsersWithInterests = (interests) => {
-  return async (dispatch) => {
-    const usersRef = firebase.firestore().collection('users');
-    await usersRef
-      .where('interests', 'array-contains-any', interests)
+export const fetchUsersWithInterests = () => {
+  return (dispatch) => {
+    const usersRef = firebase.firestore().collection('users')
+    const currentUserId = firebase.auth().currentUser.uid
+
+    firebase
+      .firestore()
+      .collection('users')
+      .doc(currentUserId)
       .get()
-      .then((snapshot) => {
-        let users = snapshot.docs.map((doc) => {
-          const data = doc.data();
-          const id = doc.id;
-          return { id, ...data };
-        });
-        dispatch(setDiscoverUsers(users));
-      });
-  };
-};
+      .then(async (snapshot) => {
+        const user = snapshot.data()
+        const interests = user.interests
+        await usersRef
+          .where('interests', 'array-contains-any', interests)
+          .get()
+          .then((snapshot) => {
+            let users = snapshot.docs.map((doc) => {
+              const data = doc.data()
+              const id = doc.id
+              return { id, ...data }
+            })
+            dispatch(setDiscoverUsers(users))
+          })
+      })
+  }
+}
 
 export default function (state = [], action) {
   switch (action.type) {
     case SET_DISCOVER_USERS:
-      return action.users;
+      return action.users
     default:
-      return state;
+      return state
   }
 }
