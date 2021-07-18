a6c1c87991d44bc79bdf271d1e77feb530702c3a
Karen Choi <49035897+DevKarenC@users.noreply.github.com>
2021-07-18 10:29:21 -0400

    Tutorial modal (#134)
    * Create a TutorialModal component
    
    * Add logic to check if the current user is a new user
    
    * feat: create a modal about the app and basics on how to use it
    
    * Modal appears only once when the user first logs in
    
    * Refactor modal logic
    
    * fix: change color of no matches message to white for readability

---
+++ src/components/TutorialModal.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Modal, View, Text, Pressable, StyleSheet } from 'react-native';
+
+export default function TutorialModal({
+  modalVisible,
+  handleRequestClose,
+  closeModal,
+}) {
+  return (
+    <Modal
+      animationType="fade"
+      visible={modalVisible}
+      onRequestClose={handleRequestClose}
+      transparent={true}
+    >
+      <View style={styles.container}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.titleText}>Welcome to Tingle!</Text>
+          <Text style={styles.bodyText}>
+            We strive to connect users based on their niche, specific interests
+            shared with others.
+          </Text>
+          <Text style={styles.bodyText}>
+            Begin by expressing your interest by double-tapping on the other
+            user's profile photo or simply tapping on the heart!
+          </Text>
+          <Text style={styles.bodyText}>
+            If they like you back, they will appear in your Matches list!
+          </Text>
+          <Pressable style={styles.button} onPress={closeModal}>
+            <Text style={styles.buttonText}>Start Tingling!</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    width: '80%',
+    height: '65%',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingHorizontal: 35,
+    paddingVertical: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  titleText: {
+    fontFamily: 'Lato_700Bold',
+    fontSize: 28,
+    letterSpacing: 0.3,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  bodyText: {
+    fontFamily: 'Lato_400Regular',
+    fontSize: 18,
+    letterSpacing: 0.1,
+    lineHeight: 26,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: '#1261B1',
+    borderRadius: 50,
+    paddingHorizontal: 20,
+    paddingVertical: 14,
+    marginTop: 10,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontFamily: 'Lato_400Regular',
+    fontSize: 18,
+    letterSpacing: 0.1,
+    textAlign: 'center',
+  },
+  logo: {
+    resizeMode: 'center',
+    width: 200,
+    height: 80,
+    marginBottom: 40,
+  },
+});
--- src/screens/HomeScreen/UsersList.js
+++ src/screens/HomeScreen/UsersList.js
@@ -1,31 +1,46 @@
-import React, { Component } from 'react'
-import { SafeAreaView, Text, ImageBackground, View } from 'react-native'
-import { ButtonGroup } from 'react-native-elements'
-import DiscoverList from './DiscoverList'
-import MatchesList from './MatchesList'
-import styles from './styles'
-import image from '../../../assets/gradient.png'
+import React, { Component } from 'react';
+import { SafeAreaView, Text, ImageBackground, View } from 'react-native';
+import { ButtonGroup } from 'react-native-elements';
+import { firebase } from '../../firebaseSpecs/config';
+import TutorialModal from '../../components/TutorialModal';
+import DiscoverList from './DiscoverList';
+import MatchesList from './MatchesList';
+import styles from './styles';
+import image from '../../../assets/gradient.png';
 
 export default class UsersList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       selectedIndex: 0,
+      modalVisible: false,
+      modalAppeared: false,
+    };
+    this.updateIndex = this.updateIndex.bind(this);
+  }
+
+  componentDidMount() {
+    const { modalAppeared } = this.state.modalAppeared;
+    const creationTime = firebase.auth().currentUser.metadata.creationTime;
+    const lastSignInTime = firebase.auth().currentUser.metadata.lastSignInTime;
+    if (modalAppeared) {
+      this.setState({ modalVisible: false });
+    } else if (!modalAppeared && creationTime === lastSignInTime) {
+      this.setState({ modalVisible: true, modalAppeared: true });
     }
-    this.updateIndex = this.updateIndex.bind(this)
   }
 
   updateIndex(selectedIndex) {
-    this.setState({ selectedIndex })
+    this.setState({ selectedIndex });
   }
 
   render() {
-    const { navigation } = this.props
-    const { selectedIndex } = this.state
+    const { navigation } = this.props;
+    const { selectedIndex, modalVisible } = this.state;
 
-    const buttonOne = () => <Text>Discover</Text>
-    const buttonTwo = () => <Text>Matches</Text>
-    const buttons = [{ element: buttonOne }, { element: buttonTwo }]
+    const buttonOne = () => <Text>Discover</Text>;
+    const buttonTwo = () => <Text>Matches</Text>;
+    const buttons = [{ element: buttonOne }, { element: buttonTwo }];
 
     return (
       <SafeAreaView style={styles.container}>
@@ -50,6 +65,11 @@ export default class UsersList extends Component {
           }}
         />
         <ImageBackground source={image} style={styles.image}>
+          <TutorialModal
+            modalVisible={modalVisible}
+            handleRequestClose={() => this.setState({ modalVisible: false })}
+            closeModal={() => this.setState({ modalVisible: false })}
+          />
           <View style={styles.flatListContainer}>
             {selectedIndex === 0 ? (
               <DiscoverList navigation={navigation} />
@@ -59,6 +79,6 @@ export default class UsersList extends Component {
           </View>
         </ImageBackground>
       </SafeAreaView>
-    )
+    );
   }
 }
