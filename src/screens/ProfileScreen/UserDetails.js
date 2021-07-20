8231fe2face29eedc3d155a1075657f1944881f5
Karen Choi <49035897+DevKarenC@users.noreply.github.com>
2021-07-20 11:06:29 -0400

    Enlarge photo (#163)
    * Create a modal to show the enlarged user profile photo
    
    * Center the image modal to screen
    
    * Position the close modal icon
    
    * Allow user's own profile photo to be enlarged
    
    * fix: only enlarge photo when photo is pressed
    
    * fix: remove defaultSource property from profile photo to avoid jump in image

--- src/screens/ProfileScreen/UserDetails.js
+++ src/screens/ProfileScreen/UserDetails.js
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { ImageBackground, View, Text } from 'react-native'
-import { Pill } from '../../components/Pill'
-import { displaySemanticPronouns } from '../../helpers'
-import { getLightColorsArray } from '../../helpers/getColorsArray'
-import { lightColors } from '../../helpers/colors.js'
-import styles from './styles'
+import React, { useEffect, useState } from 'react';
+import { ImageBackground, View, Text, Pressable } from 'react-native';
+import { Pill } from '../../components/Pill';
+import { displaySemanticPronouns } from '../../helpers';
+import { getLightColorsArray } from '../../helpers/getColorsArray';
+import { lightColors } from '../../helpers/colors.js';
+import styles from './styles';
+import EnlargedImageModel from '../SingleUserProfileScreen/EnlargedImageModal';
 
 export default function UserDetails({ user }) {
-  const { firstName, lastName, profilePicture, pronouns, interests } = user
-  const [colors, setColors] = useState([])
+  const { firstName, lastName, profilePicture, pronouns, interests } = user;
+  const [colors, setColors] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
-    const colors = getLightColorsArray(lightColors, 5)
-    setColors(colors)
-  }, [])
+    const colors = getLightColorsArray(lightColors, 5);
+    setColors(colors);
+  }, []);
 
   const renderPronouns = (pronouns) => {
     return pronouns
       .map((pronoun) => displaySemanticPronouns(pronoun))
-      .join(', ')
-  }
+      .join(', ');
+  };
 
   const renderInterests = (interests) => {
     return interests.map((interest, index) => {
       return (
         <Pill key={index} text={interest} backgroundColor={colors[index]} />
-      )
-    })
-  }
+      );
+    });
+  };
+
+  const handlePress = () => {
+    setModalVisible(!modalVisible);
+  };
 
   return (
-    <ImageBackground
-      // image source must be in {uri: linkToPhoto } format!
-      source={{ uri: profilePicture ? profilePicture : null }}
-      style={styles.image}
-      imageStyle={styles.imageStyle}
-    >
+    <>
+      <Pressable onPressIn={handlePress}>
+        <ImageBackground
+          // image source must be in {uri: linkToPhoto } format!
+          source={{ uri: profilePicture ? profilePicture : null }}
+          style={styles.image}
+          imageStyle={styles.imageStyle}
+        >
+          <EnlargedImageModel
+            user={user}
+            modalVisible={modalVisible}
+            closeModal={handlePress}
+          />
+        </ImageBackground>
+      </Pressable>
       <View style={styles.profileInfoContainer}>
         <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
           <Text style={styles.nameText}>
@@ -48,6 +63,6 @@ export default function UserDetails({ user }) {
           {renderInterests(interests)}
         </View>
       </View>
-    </ImageBackground>
-  )
+    </>
+  );
 }
